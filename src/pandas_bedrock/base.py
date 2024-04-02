043ea16c2f659bbb5ae9721a874657a8ab91e446
kongrawd <45740187+kongrawd@users.noreply.github.com>
2024-04-02 16:16:30 +0700

    Added support for text completion anthropic.claude-v2:1

--- src/pandas_bedrock/base.py
+++ src/pandas_bedrock/base.py
@@ -1,5 +1,7 @@
 import pandas
 import boto3
+import json
+from typing import List, Union
 
 class Client:
     def __init__(self, model, **kwargs) -> None:
@@ -7,18 +9,23 @@ class Client:
         self.client = self.session.client('bedrock-runtime')
         self.model = model
         
-    def ask(self, prompt: str, df: pandas.DataFrame) -> str:
-        _body = {
-            'prompt': prompt,
-            'data': df.to_dict(orient='records')
-        }
+    def ask(self, prompt: str, dfs: Union[List[pandas.DataFrame], pandas.DataFrame]) -> str:
+        _dumps_dfs = json.dumps(dfs.to_dict(orient='records')) if isinstance(dfs, pandas.DataFrame) else json.dumps([df.to_dict(orient='records') for df in dfs])
+        print(_dumps_dfs)
+        _prompt = """\n\nHuman: Analyze the following dataframe(s) {} \
+            \n\n{} \
+            \n\nAssistant:""".format(_dumps_dfs, prompt)
+        _body = json.dumps({
+            'prompt': _prompt,
+            'max_tokens_to_sample': 2048
+        })
         try:
             resp = self.client.invoke_model(
                 body=_body,
                 contentType='application/json',
                 accept='application/json',
                 modelId=self.model
-            )
+            ).get('body').read().decode('utf-8')
         except Exception as e:
             raise e
-        return resp['body'].read().decode('utf-8')
+        return json.loads(resp).get('completion')
--- tests/test_base.py
+++ tests/test_base.py
@@ -9,5 +9,47 @@ def test_client(target_env):
         region_name=target_env['aws_region_name'],
         profile_name=target_env['aws_profile_name']
         )
-    df = pandas.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
-    assert client.ask('prompt', df) == 'response'
+    df = pandas.DataFrame({'goat': [1, 2, 3], 'cow': [4, 5, 6]})
+    assert 'cow' in client.ask('Respond strictly one word that\'s either goat or cow, what\'s the second column\'s word', df)
+
+def test_dataframe_ask(target_env):
+    client = pandas_bedrock.Client(
+        model=target_env['model_id'],
+        region_name=target_env['aws_region_name'],
+        profile_name=target_env['aws_profile_name']
+        )
+    df = pandas.DataFrame({
+        'Name': ['Scooby Doo', 'Lassie', 'Snoopy', 'Pluto', 'Santa’s Little Helper'],
+        'Breed': ['Great Dane', 'Collie', 'Beagle', 'Mixed', 'Greyhound'],
+        'Show': ['Scooby-Doo', 'Lassie', 'Peanuts', 'Disney', 'The Simpsons']
+    })
+    resp = client.ask('explain', df)
+    print(resp)
+    assert 'dog' in resp.lower()
+
+def test_dataframe_ask_list(target_env):
+    client = pandas_bedrock.Client(
+        model=target_env['model_id'],
+        region_name=target_env['aws_region_name'],
+        profile_name=target_env['aws_profile_name']
+        )
+    df1 = pandas.DataFrame({
+        'Prompt_ID': [1, 2, 3],
+        'Prompt': [
+            'Write a poem about the autumn season.',
+            'Explain the theory of relativity in simple terms.',
+            'Create a short story about a space-faring cat.'
+        ]
+    })
+    df2 = pandas.DataFrame({
+        'Response_ID': [1, 2, 3],
+        'Prompt_ID': [1, 2, 3],
+        'Response': [
+            'Leaves in blaze, fall\'s firmament prays, as hues cascade, a seasonal serenade.',
+            'Einstein\'s theory talks of time and space, saying speed can face, a relative embrace.',
+            'Cosmo the cat, with a helmet so snug, leaped into the black, a starry hug.'
+        ]
+    })
+    resp = client.ask('Explain the theory of relativity in simple terms.', [df1, df2])
+    print(resp)
+    assert 'einstein' in resp.lower()
